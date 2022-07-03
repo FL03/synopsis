9cea1b102613e2d02e07c034e99ac944ba3dd602
FL03 <jo3mccain@icloud.com>
2022-07-03 16:21:18 -0500

    Update

--- src/synopsis/__init__.py
+++ src/synopsis/__init__.py
@@ -1,3 +1,2 @@
-__import__("__main__")
 __package__ = "synopsis"
 __version__ = '0.1.0'
--- src/synopsis/__main__.py
+++ src/synopsis/__main__.py
@@ -1,3 +1,3 @@
 if __name__ == "__main__":
-    from app.interface import run
+    from synopsis.app.interface import run
     run()
